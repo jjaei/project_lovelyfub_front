6d12002aa029eaaf8491febf66b6c7087dfe40c7
daeun088 <kde2329@naver.com>
2023-07-26 00:27:00 +0900

    식당페이지 모달창 구현 완료. 공유 기능 미완, 마켓 페이지 수정 전

--- src/components/Cafe/Cafe.jsx
+++ src/components/Cafe/Cafe.jsx
@@ -1,44 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Cafe.module.scss";
+import { useEffect, useRef } from 'react';
+import CafeModal from "./CafeModal"; // CafeModal 컴포넌트를 불러옵니다.
 
 function Cafe() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const mapElement = useRef(null);
+  const mapInstance = useRef(null);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  useEffect(() => {
+    const { naver } = window;
+    if (!mapElement.current || !naver) return;
+
+    // Create or update the map instance
+    if (!mapInstance.current) {
+      // 지도에 표시할 위치의 위도와 경도 좌표를 파라미터로 넣어줍니다.
+      const location = new naver.maps.LatLng(37.5656, 126.9769);
+      const mapOptions = {
+        center: location,
+        zoom: 17,
+        zoomControl: false,
+      };
+      mapInstance.current = new naver.maps.Map(mapElement.current, mapOptions);
+      new naver.maps.Marker({
+        position: location,
+        map: mapInstance.current,
+      });
+    } else {
+      // If the map instance already exists, just reposition the marker
+      const location = new naver.maps.LatLng(37.5656, 126.9769);
+      mapInstance.current.setCenter(location);
+      mapInstance.current.markers[0].setPosition(location);
+    }
+  }, [isModalOpen]); // Run this effect whenever isModalOpen changes
+
+  useEffect(() => {
+    // Clean up the map instance when the component unmounts
+    return () => {
+      if (mapInstance.current) {
+        mapInstance.current.destroy();
+        mapInstance.current = null;
+      }
+    };
+  }, []);
+
   return (
     <div className={styles.layout}>
       <div className={styles.title}>푸드리퍼브 재료로<br/>음식을 만드는 식당이에요</div>
 
       <div className={styles.cafeContainer}>
-          <div className={styles.cafeList}>
+          <div className={styles.cafeList} onClick={toggleModal}>
             <img src="product.jpg" className={styles.productImage} />
             <div className={styles.productTitle}>소일 보울앤부즈</div>
             <div className={styles.productText}>푸드리퍼브 식재료로 만드는<br/>캐주얼 다이닝 레스토랑</div>
           </div>
-          <div className={styles.cafeList}>
+          <div className={styles.cafeList} onClick={toggleModal}>
             <img src="product.jpg" className={styles.productImage} />
             <div className={styles.productTitle}>소일 보울앤부즈</div>
             <div className={styles.productText}>푸드리퍼브 식재료로 만드는<br/>캐주얼 다이닝 레스토랑</div>
           </div>
-          <div className={styles.cafeList}>
+          <div className={styles.cafeList} onClick={toggleModal}>
             <img src="product.jpg" className={styles.productImage} />
             <div className={styles.productTitle}>소일 보울앤부즈</div>
             <div className={styles.productText}>푸드리퍼브 식재료로 만드는<br/>캐주얼 다이닝 레스토랑</div>
           </div>
-          <div className={styles.cafeList}>
+          <div className={styles.cafeList} onClick={toggleModal}>
             <img src="product.jpg" className={styles.productImage} />
             <div className={styles.productTitle}>소일 보울앤부즈</div>
             <div className={styles.productText}>푸드리퍼브 식재료로 만드는<br/>캐주얼 다이닝 레스토랑</div>
           </div>
-          <div className={styles.cafeList}>
+          <div className={styles.cafeList} onClick={toggleModal}>
             <img src="product.jpg" className={styles.productImage} />
             <div className={styles.productTitle}>소일 보울앤부즈</div>
             <div className={styles.productText}>푸드리퍼브 식재료로 만드는<br/>캐주얼 다이닝 레스토랑</div>
           </div>
-          <div className={styles.cafeList}>
+          <div className={styles.cafeList} onClick={toggleModal}>
             <img src="product.jpg" className={styles.productImage} />
             <div className={styles.productTitle}>소일 보울앤부즈</div>
             <div className={styles.productText}>푸드리퍼브 식재료로 만드는<br/>캐주얼 다이닝 레스토랑</div>
           </div>
         </div>
 
+      {isModalOpen && (
+        <CafeModal closeModal={toggleModal} mapInstance={mapInstance} toggleModal={toggleModal} />
+      )}
     </div>
   );
 }
--- src/components/Market/Market.jsx
+++ src/components/Market/Market.jsx
@@ -1,50 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Market.module.scss";
 
 function Market() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <div className={styles.layout}>
-      <div className={styles.title}>푸드리퍼브 재료를 <br/>판매하는 마켓이에요</div>
+      <div className={styles.title}>푸드리퍼브 재료를<br/> 판매하는 마켓이에요</div>
 
       <div className={styles.cafeContainer}>
-          <div className={styles.cafeList}>
-            <img src="market.jpg" className={styles.productImage} />
-            <div className={styles.productTitle}>홈마켓</div>
-            <div className={styles.productText}>푸드리퍼브 식재료를 판매하는<br/>
-떠오르는 트렌디한 상점</div>
-          </div>
-          <div className={styles.cafeList}>
-            <img src="market.jpg" className={styles.productImage} />
-            <div className={styles.productTitle}>홈마켓</div>
-            <div className={styles.productText}>푸드리퍼브 식재료를 판매하는<br/>
-떠오르는 트렌디한 상점</div>
-          </div>
-          <div className={styles.cafeList}>
-            <img src="market.jpg" className={styles.productImage} />
-            <div className={styles.productTitle}>홈마켓</div>
-            <div className={styles.productText}>푸드리퍼브 식재료를 판매하는<br/>
-떠오르는 트렌디한 상점</div>
-          </div>
-          <div className={styles.cafeList}>
-            <img src="market.jpg" className={styles.productImage} />
-            <div className={styles.productTitle}>홈마켓</div>
-            <div className={styles.productText}>푸드리퍼브 식재료를 판매하는<br/>
-떠오르는 트렌디한 상점</div>
-          </div>
-          <div className={styles.cafeList}>
-            <img src="market.jpg" className={styles.productImage} />
-            <div className={styles.productTitle}>홈마켓</div>
-            <div className={styles.productText}>푸드리퍼브 식재료를 판매하는<br/>
-떠오르는 트렌디한 상점</div>
-          </div>
-          <div className={styles.cafeList}>
-            <img src="market.jpg" className={styles.productImage} />
-            <div className={styles.productTitle}>홈마켓</div>
-            <div className={styles.productText}>푸드리퍼브 식재료를 판매하는<br/>
-떠오르는 트렌디한 상점</div>
+        <div className={styles.cafeList} onClick={toggleModal}>
+          <img src="market.jpg" className={styles.productImage} />
+          <div className={styles.productTitle}>홈마켓</div>
+          <div className={styles.productText}>푸드리퍼브 식재료를 판매하는<br/>떠오르는 트렌디한 상점</div>
+        </div>
+        <div className={styles.cafeList} onClick={toggleModal}>
+          <img src="market.jpg" className={styles.productImage} />
+          <div className={styles.productTitle}>홈마켓</div>
+          <div className={styles.productText}>푸드리퍼브 식재료를 판매하는<br/>떠오르는 트렌디한 상점</div>
+        </div>
+        <div className={styles.cafeList} onClick={toggleModal}>
+          <img src="market.jpg" className={styles.productImage} />
+          <div className={styles.productTitle}>홈마켓</div>
+          <div className={styles.productText}>푸드리퍼브 식재료를 판매하는<br/>떠오르는 트렌디한 상점</div>
+        </div>
+        <div className={styles.cafeList} onClick={toggleModal}>
+          <img src="market.jpg" className={styles.productImage} />
+          <div className={styles.productTitle}>홈마켓</div>
+          <div className={styles.productText}>푸드리퍼브 식재료를 판매하는<br/>떠오르는 트렌디한 상점</div>
+        </div>
+        <div className={styles.cafeList} onClick={toggleModal}>
+          <img src="market.jpg" className={styles.productImage} />
+          <div className={styles.productTitle}>홈마켓</div>
+          <div className={styles.productText}>푸드리퍼브 식재료를 판매하는<br/>떠오르는 트렌디한 상점</div>
+        </div>
+        <div className={styles.cafeList} onClick={toggleModal}>
+          <img src="market.jpg" className={styles.productImage} />
+          <div className={styles.productTitle}>홈마켓</div>
+          <div className={styles.productText}>푸드리퍼브 식재료를 판매하는<br/>떠오르는 트렌디한 상점</div>
+        </div>
+      </div>
+
+      {/* The modal */}
+      {isModalOpen && (
+        <div className={styles.modalBackdrop}>
+          <div className={styles.modal}>
+            {/* 상단 바 */}
+            <span className={styles.closeButton} onClick={toggleModal}>
+              &times;
+            </span>
+            <span className={styles.shareButton}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
+              <path d="M17.8405 3C19.4467 3 20.7487 4.34258 20.7487 5.99872C20.7487 7.65487 19.4467 8.99743 17.8405 8.99743C17.0235 8.99743 16.2853 8.65011 15.757 8.09085L10.4635 11.2111C10.5298 11.4625 10.5651 11.727 10.5651 12C10.5651 12.273 10.5298 12.5375 10.4635 12.7888L15.7578 15.9084C16.2859 15.3496 17.0239 15.0026 17.8405 15.0026C19.4467 15.0026 20.7487 16.3451 20.7487 18.0013C20.7487 19.6574 19.4467 21 17.8405 21C16.2343 21 14.9323 19.6574 14.9323 18.0013C14.9323 17.7283 14.9677 17.4637 15.034 17.2124L9.74043 14.0921C9.21216 14.6513 8.47389 14.9987 7.65694 14.9987C6.05078 14.9987 4.74873 13.6561 4.74873 12C4.74873 10.3438 6.05078 9.00128 7.65694 9.00128C8.47352 9.00128 9.2115 9.34831 9.73973 9.90713L15.034 6.7876C14.9677 6.53624 14.9323 6.27174 14.9323 5.99872C14.9323 4.34258 16.2343 3 17.8405 3Z" fill="#313131"/>
+              </svg>
+            </span>
+            <span className={styles.heart}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
+                <path d="M13.5686 5.57912L12.7479 6.40163L11.9246 5.57838C9.82561 3.47931 6.42234 3.47931 4.32328 5.57838C2.22421 7.67744 2.22421 11.0807 4.32328 13.1798L12.2186 21.0751C12.5115 21.368 12.9864 21.368 13.2793 21.0751L21.1807 13.1783C23.2751 11.0723 23.2787 7.67857 21.1793 5.57912C19.0764 3.47623 15.6715 3.47623 13.5686 5.57912ZM20.1171 12.1206L12.7489 19.4842L5.38394 12.1191C3.87065 10.6058 3.87065 8.15232 5.38394 6.63904C6.89722 5.12575 9.35073 5.12575 10.864 6.63904L12.2214 7.99648C12.5193 8.29435 13.004 8.28854 13.2946 7.98363L14.6293 6.63978C16.1464 5.12268 18.6015 5.12268 20.1186 6.63978C21.6323 8.15343 21.6297 10.5997 20.1171 12.1206Z" fill="#313131"/>
+              </svg>
+            </span>
+
+            {/*모달창 내용*/}
+            <img src="market.jpg" className={styles.modalImage} />
+            <div className={styles.modalTitle}>홈마켓</div>
+            <div className={styles.productText}>푸드리퍼브 식재료를 판매하는<br/>떠오르는 트렌디한 상점</div>
+            <h2>모달 창</h2>
+            <p>모달 내용을 여기에 작성합니다.</p>
           </div>
         </div>
-
+      )}
     </div>
   );
 }
